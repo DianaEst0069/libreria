ba9a81a19c763b491c69d30c02f81d415f9f343b
DianaEst0069 <ecdo230069@upemor.edu.mx>
2026-03-31 13:14:17 -0600

    email

---
+++ app/Listeners/EnviarCorreo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+use Illuminate\Auth\Events\Login;
+class EnviarCorreo
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        //Obtener la información del usuario en cuento inicia sesión 
+        $user = $event->user;
+
+        Mail::to($user->email())->send(new AlertaLoginCorreo($user));
+    }
+}
